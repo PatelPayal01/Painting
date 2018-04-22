753f2b8c8c2585265d1910f0947e57353fa97cf8
Payal Patel <33231522+PatelPayal01@users.noreply.github.com>
2018-04-22 17:32:21 +0530

    responsive

--- src/app/paintings/viewed-painting/viewed-painting.component.ts
+++ src/app/paintings/viewed-painting/viewed-painting.component.ts
@@ -7,11 +7,15 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 })
 export class ViewedPaintingComponent implements OnInit {
 
+  smallScreen: boolean;
   @Input() viewedPainting: string;
   @Output() close: EventEmitter<any> = new EventEmitter();
   constructor() { }
 
   ngOnInit() {
+    if (window.screen.width <= 750) {
+      this.smallScreen = true;
+    }
   }
 
   closed() {
